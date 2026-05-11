ace7fc745d11b204f4f78eb59d22335385f2283c
MJ Studio <mym0404@gmail.com>
2026-05-11 16:55:09 +0900

    [Parser Support] SE4 wrapping paragraph parser (#9)

--- src/parsing/naver-blog/se4/blocks/WrappingParagraphBlock.ts
+++ src/parsing/naver-blog/se4/blocks/WrappingParagraphBlock.ts
@@ -3,31 +3,42 @@ import { LeafBlock } from "../../core/BaseBlock.js"
 import { parseImageLink, se4ImageLinkSelector } from "./ImageLink.js"
 import { parseTextBlocks } from "./TextBlock.js"
 
+const wrappingParagraphLayoutClasses = ["se-l-inner-big-left", "se-l-inner-big-right"]
+
 export class NaverSe4WrappingParagraphBlock extends LeafBlock {
   override readonly id = "wrappingParagraph"
   override readonly label = "감싸는 문단"
 
   override match({ $node }: ParserBlockContext) {
-    return $node.hasClass("se-wrappingParagraph") && $node.hasClass("se-l-inner-big-right")
+    return (
+      $node.hasClass("se-wrappingParagraph") &&
+      wrappingParagraphLayoutClasses.some((className) => $node.hasClass(className))
+    )
   }
 
   override convert({ $node, options }: Parameters<LeafBlock["convert"]>[0]) {
-    const image = parseImageLink(
-      $node.find(".se-component-slot-float").find(se4ImageLinkSelector).first(),
-    )
+    const $imageSlot = $node.find(".se-component-slot-float").first()
+    const imageBlocks = []
 
-    if (!image) {
-      throw new Error("SE4 wrapping paragraph image parsing failed.")
+    if ($imageSlot.length > 0) {
+      const image = parseImageLink($imageSlot.find(se4ImageLinkSelector).first())
+
+      if (!image) {
+        throw new Error("SE4 wrapping paragraph image parsing failed.")
+      }
+
+      imageBlocks.push({ type: "image" as const, image })
     }
 
     const $textSlot = $node.find(".se-component-slot").not(".se-component-slot-float").first()
+    const textBlocks =
+      $textSlot.length > 0
+        ? parseTextBlocks({
+            $node: $textSlot,
+            options,
+          })
+        : []
 
-    return [
-      { type: "image" as const, image },
-      ...parseTextBlocks({
-        $node: $textSlot,
-        options,
-      }),
-    ]
+    return [...imageBlocks, ...textBlocks]
   }
 }
